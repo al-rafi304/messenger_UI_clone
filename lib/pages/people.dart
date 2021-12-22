c2937a45d08dc1b3c225ef04f57d35fb51317d73
Mohammad Al Rafi <arafi304@gmail.com>
2021-12-22 21:45:15 +0600

    Modified People page

--- lib/pages/people.dart
+++ lib/pages/people.dart
@@ -4,19 +4,79 @@ class People extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(
-          "Active Peoples",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Active",
+                  style: TextStyle(color: Colors.white, ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-      body: Center(
-          child: Text(
-        "People page",
-        style: TextStyle(color: Colors.white),
-      )),
-    );
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                PeoplesItem(name: 'Rk Biplob', picPath: 'assets/03.jpg'),
+                Divider(
+                  color: Colors.grey[900],
+                  thickness: 1,
+                  height: 20,
+                  indent: 50,
+                ),
+                PeoplesItem(name: 'Rk Biplob', picPath: 'assets/03.jpg'),
+                Divider(
+                  color: Colors.grey[900],
+                  thickness: 1,
+                  height: 20,
+                  indent: 50,
+                ),
+                PeoplesItem(name: 'Rk Biplob', picPath: 'assets/03.jpg'),
+                Divider(
+                  color: Colors.grey[900],
+                  thickness: 1,
+                  height: 20,
+                  indent: 50,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class PeoplesItem extends StatelessWidget {
+  final String picPath;
+  final String name;
+
+  PeoplesItem({this.picPath, this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(picPath),
+        ),
+        SizedBox(
+          width: 12,
+        ),
+        Text(
+          name,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ],
+    ));
   }
 }
