ab6ef32a085542d2ac1a039c177da5a124543d85
Mohammad Al Rafi <arafi304@gmail.com>
2021-12-23 15:48:53 +0600

    Added new class for avatar bubbles
    This class can show active status and story status but it doesn't contain names

---
+++ lib/AvatarBubble.dart
@@ -0,0 +1,59 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class AvatarBubble extends StatelessWidget {
+
+  final String picPath;
+  final double radius;
+  final bool isActive;
+  final bool hasStory;
+
+  AvatarBubble({this.picPath, this.radius, this.isActive = false, this.hasStory = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: hasStory ? CircleAvatar(     // Blue Border
+        radius: radius,
+        backgroundColor: Colors.blue,
+        child: CircleAvatar(              // Black Border
+          backgroundColor: Colors.black,
+          radius: radius - 2,
+          child: CircleAvatar(              // Profile Pic
+            backgroundImage: AssetImage(picPath),
+            radius: radius - 4.5,
+            child: isActive ? Container(
+              alignment: Alignment.bottomRight,
+              child: CircleAvatar(          // Active Black Border
+                backgroundColor: Colors.black,
+                radius: 8.5,
+                child: CircleAvatar(        // Active Green fill
+                  backgroundColor: Colors.green,
+                  radius: 6,
+                ),
+              ),
+            )
+            : Text(''),
+          ),
+        ),
+      )
+      : CircleAvatar(
+        radius: radius,
+        backgroundImage: AssetImage(picPath),
+        child: isActive ? Container(
+            alignment: Alignment.bottomRight,
+            child: CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 8.5,
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 6,
+              ),
+            ),
+          )
+          : Text(''),
+      )
+    );
+  }
+}
